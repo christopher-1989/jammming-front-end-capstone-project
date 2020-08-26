c4d16703a84a7c592f84578034a30557af51f625
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-08-26 13:04:12 +1000

    Added all component directories and files

---
+++ src/components/Tracklist/Tracklist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Tracklist.css';
+
+
+class Tracklist extends React.Component {
+    render() {
+      return (
+        <div class="TrackList">
+            <!-- You will add a map method that renders a set of Track components  -->
+        </div>
+      );
+    }
+  }
+
+  export default Tracklist;
