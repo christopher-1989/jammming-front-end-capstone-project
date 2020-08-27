327354f97f320e6e0e938d29814646bfb3cf9d83
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-08-27 17:53:30 +1000

    functioning up to step 56

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,21 +24,20 @@ import Playlist from '../Playlist/Playlist';
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
   }
+
   addTrack(track) {
-    let playlistTRKS = this.state.playlistTracks.some(singleTrack => singleTrack.id === track.id)
-    if (playlistTRKS === false) {
-      this.state.playlistTracks.push(track)
-      this.setState({playlistTracks: this.state.playlistTracks})
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
     }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks })
   }
+
   removeTrack(track) {
-    //Step 49
-    let updatedPlaylist = this.state.playlistTracks.map(singleTrack => {
-      if (singleTrack.id !== track.id) {
-        return singleTrack
-      }
-    })
-    this.setState({playlistTracks: updatedPlaylist})
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(singleTrack => singleTrack.id !== track.id); 
+    this.setState({ playlistTracks: tracks })
   }
   render() {
     return (
@@ -47,9 +46,8 @@ import Playlist from '../Playlist/Playlist';
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} 
-                            onAdd={this.addTrack}/>
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
